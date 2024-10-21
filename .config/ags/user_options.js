fd1e6631035b76fc1df9dc1a0aedcda9fda150fd
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2024-10-21 16:08:19 +0530

    modified as per personal prefs

--- .config/ags/user_options.js
+++ .config/ags/user_options.js
@@ -4,6 +4,53 @@
 //   options listed in this file will override the default ones in the above file
 
 const userConfigOptions = {
+    'overview': {
+        'scale': 0.16, // Relative to screen size
+        'numOfRows': 5,
+        'numOfCols': 5,
+    },
+    'sidebar': {
+        'pages': {
+            'order': ["apis", "tools"],
+            'apis': {
+                'order': ["gemini", "gpt"],
+            }
+        },
+    },
+    'time': {
+        // See https://docs.gtk.org/glib/method.DateTime.format.html
+        // Here's the 12h format: "%I:%M%P"
+        // For seconds, add "%S" and set interval to 1000
+        'format': "%I:%M",
+        'interval': 5000,
+        'dateFormatLong': "%A, %d/%m", // On bar
+        'dateInterval': 5000,
+        'dateFormat': "%d/%m", // On notif time
+    },
+    'weather': {
+        'city': "Ahmedabad",
+        'preferredUnit': "C", // Either C or F
+    },
+    'dock': {
+        'enabled': false,
+        'hiddenThickness': 5,
+        'pinnedApps': [],
+        'layer': 'top',
+        'monitorExclusivity': true, // Dock will move to other monitor along with focus if enabled
+        'searchPinnedAppIcons': false, // Try to search for the correct icon if the app class isn't an icon name
+        'trigger': ['client-added', 'client-removed'], // client_added, client_move, workspace_active, client_active
+        // Automatically hide dock after `interval` ms since trigger
+        'autoHide': [
+            {
+                'trigger': 'client-added',
+                'interval': 500,
+            },
+            {
+                'trigger': 'client-removed',
+                'interval': 500,
+            },
+        ],
+    },
 }
 
 export default userConfigOptions;
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,6 +19,57 @@ end
 
 alias pamcan=pacman
 
+
+# Common use
+alias .. 'cd ..'
+alias ... 'cd ../..'
+alias .... 'cd ../../..'
+alias ..... 'cd ../../../..'
+alias ...... 'cd ../../../../..'
+alias big 'expac -H M "%m\t%n" | sort -h | nl'     # Sort installed packages according to size in MB (expac must be installed)
+alias dir 'dir --color=auto'
+alias fixpacman 'sudo rm /var/lib/pacman/db.lck'
+alias gitpkg 'pacman -Q | grep -i "\-git" | wc -l' # List amount of -git packages
+alias grep 'ugrep --color=auto'
+alias egrep 'ugrep -E --color=auto'
+alias fgrep 'ugrep -F --color=auto'
+alias grubup 'sudo update-grub'
+alias hw 'hwinfo --short'                          # Hardware Info
+alias ip 'ip -color'
+alias psmem 'ps auxf | sort -nr -k 4'
+alias psmem10 'ps auxf | sort -nr -k 4 | head -10'
+alias rmpkg 'sudo pacman -Rdd'
+alias tarnow 'tar -acf '
+alias untar 'tar -zxvf '
+alias upd '/usr/bin/garuda-update'
+alias vdir 'vdir --color=auto'
+alias wget 'wget -c '
+
+# Get fastest mirrors
+alias mirror 'sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist'
+alias mirrora 'sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist'
+alias mirrord 'sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist'
+alias mirrors 'sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist'
+
+# Help people new to Arch
+alias apt 'man pacman'
+alias apt-get 'man pacman'
+alias please 'sudo'
+alias tb 'nc termbin.com 9999'
+alias helpme 'echo "To print basic information about a command use tldr <command>"'
+alias pacdiff 'sudo -H DIFFPROG=meld pacdiff'
+
+# Get the error messages from journalctl
+alias jctl 'journalctl -p 3 -xb'
+
+# Recent installed packages
+alias rip 'expac --timefmt="%Y-%m-%d %T" "%l\t%n %v" | sort | tail -200 | nl'
+alias q 'exit'
+alias c 'clear'
+alias xcp 'xclip -selection clipboard'
+alias vdata 'sudo chmod -R 777 .local/share/waydroid/data/media/'
+alias my 'clear && fastfetch'
+
 # function fish_prompt
 #   set_color cyan; echo (pwd)
 #   set_color green; echo '> '
